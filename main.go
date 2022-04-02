a63c86f6bfdaf6973f8dda7d78632548f31b2ed3
gzq <516605659@qq.com>
2022-04-02 15:21:55 +0800

    go mod init

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"MyGoProject/Go-Tools/utils"
+	//"MyGoProject/Go-Tools/utils"
 	"fmt"
+	"github.com/silenceGuo/Go-Tools/utils"
 )
 
-func main(){
+func main() {
 	f := *utils.ReadFilebufe("C:\\Users\\Administrator\\Desktop\\startServerdev1.yml")
-	for _,v:=range f{
-		fmt.Printf("%s",string(v))
+	for _, v := range f {
+		fmt.Printf("%s", string(v))
 	}
 	var s []string
-	s = append(s, "1","12easdad")
-    utils.WriteFile("C:\\Users\\Administrator\\Desktop\\1.TXT",3)
+	s = append(s, "1", "12easdad")
+	utils.WriteFile("C:\\Users\\Administrator\\Desktop\\1.TXT", 3)
 	fmt.Println(utils.PathExists("C:\\Users\\Administrator\\Desktop\\1.TXT1"))
 }
